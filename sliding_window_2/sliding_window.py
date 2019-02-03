31f85cc0fb1d24d7eaead1492a632233ce77da9b
rizkidewanto <46876786+rizkidewanto@users.noreply.github.com>
2019-02-03 17:47:24 +0700

    Update sliding_window.py

--- sliding_window_2/sliding_window.py
+++ sliding_window_2/sliding_window.py
@@ -1,12 +1,14 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+from keras.models import load_model
 
 # read the image and define the stepSize and window size 
 # (width,height)
 image = cv2.imread('pisang.jpg') # your image path
 tmp = image # for drawing a rectangle
 stepSize = 50
+model=load_model('classifier.h5')
 (w_width, w_height) = (50, 50) # window size
 for x in range(0, image.shape[1] - w_width , stepSize):
     for y in range(0, image.shape[0] - w_height, stepSize):
@@ -14,7 +16,10 @@ for x in range(0, image.shape[1] - w_width , stepSize):
 
         # classify content of the window with your classifier and 
         # determine if the window includes an object (cell) or not
-
+        window = np.expand_dims(window, axis=0)
+        prediksi = model.predict(window)
+        prediksi = np.argmax(prediksi, axis=1)
+        
         # draw window on image
         cv2.rectangle(tmp, (x, y), (x + w_width, y + w_height), (255, 0, 0), 2) # draw rectangle on image
         plt.imshow(np.array(tmp).astype('uint8'))
