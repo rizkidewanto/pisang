a3d609a2919b95696f4beaa9fe89d146498f9046
rizkidewanto <46876786+rizkidewanto@users.noreply.github.com>
2019-02-08 21:08:04 +0700

    Add file

--- sliding_window_2/sliding_window.py
+++ sliding_window_2/sliding_window.py
@@ -8,7 +8,7 @@ from keras.models import load_model
 image = cv2.imread('pisang.jpg') # your image path
 tmp = image # for drawing a rectangle
 stepSize = 50
-model=load_model('classifier_50.h5') #Belum diganti model utk ukuran 10
+model=load_model('classifier_10.h5') #Belum diganti model utk ukuran 10
 (w_width, w_height) = (50, 50) # window size
 for x in range(0, image.shape[1] - w_width , stepSize):
     for y in range(0, image.shape[0] - w_height, stepSize):
