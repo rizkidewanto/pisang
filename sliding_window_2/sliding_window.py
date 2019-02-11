bf6754e84b319c9c869e4e3b870a8367012df71b
rizkidewanto <46876786+rizkidewanto@users.noreply.github.com>
2019-02-11 14:35:55 +0700

    Update sliding_window.py

--- sliding_window_2/sliding_window.py
+++ sliding_window_2/sliding_window.py
@@ -8,7 +8,7 @@ from keras.models import load_model
 image = cv2.imread('pisang.jpg') # your image path
 tmp = image # for drawing a rectangle
 stepSize = 50
-model=load_model('classifier_10.h5') #Belum diganti model utk ukuran 10
+model=load_model('classifier_10.h5')
 (w_width, w_height) = (50, 50) # window size
 for x in range(0, image.shape[1] - w_width , stepSize):
     for y in range(0, image.shape[0] - w_height, stepSize):
@@ -17,7 +17,7 @@ for x in range(0, image.shape[1] - w_width , stepSize):
         window = np.expand_dims(window, axis=0)
         prediksi = model.predict(window)
         prediksi = np.argmax(prediksi, axis=1)
-        print(prediksi)
+        print(prediksi) #For Troubleshoot
         #cv2.rectangle(tmp, (x, y), (x + w_width, y + w_height), (0, 0, 255), 2)
         #if prediksi==0:
         #    cv2.rectangle(tmp, (x, y), (x + w_width, y + w_height), (255, 0, 0), 2)
@@ -26,7 +26,6 @@ for x in range(0, image.shape[1] - w_width , stepSize):
         #elif prediksi==2:
         #    cv2.rectangle(tmp, (x, y), (x + w_width, y + w_height), (0, 0, 255), 2)
         plt.imshow(np.array(tmp).astype('uint8'))
-# show all windows
 #plt.show()
 cv2.imshow('Gambar',np.array(tmp).astype('uint8'))
 cv2.waitKey(0)
